e65cad41748687b22425234012365dd8ea3dd6ad
choyos <cesar91hoyos@gmail.com>
2015-10-19 11:16:48 +0200

    Cambios

--- header.php
+++ header.php
@@ -4,7 +4,7 @@ session_start();
 
 if(empty($_SESSION) && $title != "Acceso al portal"){
   header('Location: /');
-  die();
+  //die();
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
